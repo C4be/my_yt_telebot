6f9cff6b20adf94dcffa1c9bfd9524d8506c45e7
C4be <dmitryc4be@gmail.com>
2025-10-20 22:14:17 +0300

    add logging on basic handlers

--- src/tg/app/handlers/basic_commands/help.py
+++ src/tg/app/handlers/basic_commands/help.py
@@ -1,11 +1,20 @@
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
+import logging
+
+# Настройка логгера
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - [%(levelname)s] - %(name)s - %(message)s",
+)
+logger = logging.getLogger(__name__)
 
 router = Router()
 
 @router.message(Command('help'))
 async def start_handler(message: Message) -> None:
+    logging.INFO(f'\help команда от {message.from_user.id}')
     await message.answer(
         "Я пока не написал этот раздел"
     )
--- src/tg/app/handlers/basic_commands/start.py
+++ src/tg/app/handlers/basic_commands/start.py
@@ -1,11 +1,22 @@
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
+import logging
+
 
 router = Router()
 
+# Настройка логгера
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - [%(levelname)s] - %(name)s - %(message)s",
+)
+logger = logging.getLogger(__name__)
+
+
 @router.message(Command('start'))
 async def start_handler(message: Message) -> None:
+    logging.INFO(f'\help команда от {message.from_user.id}')
     await message.answer(
         "Привет, я бот, через который ты сможешь грузить и загружать видео на YT/TT автоматически!"
     )
